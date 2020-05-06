5afe8f32016a5edd2f2f2a210412a873cc25b29b
bgassefa <44286093+Bgassefa@users.noreply.github.com>
2020-05-06 17:55:09 -0400

    Build Server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser'); 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+app.get('/api', (req, res) => {
+ 
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/mnkf-cu5c.json';
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((r) => r.map((dataset) => dataset))
+    .then((data) => {
+      console.log(data);
+      res.send({ data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/error');
+    });
+});
+
+app.post('/api/post', (req, res) => {
+  res.json({
+    message: 'POST'
+  });
+});
+
+app.put('/api/put', (req, res) => {
+  res.json({
+    message: 'PUT'
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
