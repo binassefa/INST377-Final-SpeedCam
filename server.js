a2103935f4bcc418673430f706e2e04ba2651239
bgassefa <44286093+Bgassefa@users.noreply.github.com>
2020-05-12 04:00:20 -0400

    Last minute revisions

--- server.js
+++ server.js
@@ -6,7 +6,26 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('/public'));
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.get('/', function (req, res) {
+  res.sendFile('/index.html', { root: '.' })
+});
+app.get('/index.html', function (req, res) {
+  res.sendFile('/index.html', { root: '.' })
+});
+app.get('/about.html', function (req, res) {
+  res.sendFile('public/about.html', { root: '.' })
+});
+app.get('/data.html', function (req, res) {
+  res.sendFile('public/data.html', { root: '.' })
+});
+app.get('/documentation.html', function (req, res) {
+  res.sendFile('public/documentation.html', { root: '.' })
+});
+
 
 app.get('/api', (req, res) => {
  
